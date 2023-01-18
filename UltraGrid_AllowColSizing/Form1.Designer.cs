6dc25ab42a1643838337ded926ce42dd7d41f000
mkamiishi <mkamiishi@infragistics.com>
2023-01-18 17:16:26 +0900

    初回コミット
    Initial commit

---
+++ UltraGrid_AllowColSizing/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace UltraGrid_AllowColSizing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ultraGrid1 = new Infragistics.Win.UltraWinGrid.UltraGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.ultraGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ultraGrid1
+            // 
+            this.ultraGrid1.Location = new System.Drawing.Point(13, 13);
+            this.ultraGrid1.Name = "ultraGrid1";
+            this.ultraGrid1.Size = new System.Drawing.Size(844, 514);
+            this.ultraGrid1.TabIndex = 0;
+            this.ultraGrid1.Text = "ultraGrid1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 539);
+            this.Controls.Add(this.ultraGrid1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ultraGrid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Infragistics.Win.UltraWinGrid.UltraGrid ultraGrid1;
+    }
+}
+
---
+++ UltraGrid_AllowColSizing/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UltraGrid_AllowColSizing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ultraGrid1.DataSource = GetData();
+
+            ultraGrid1.DisplayLayout.Override.AllowColSizing = Infragistics.Win.UltraWinGrid.AllowColSizing.Free;
+
+            ultraGrid1.DisplayLayout.Bands[0].Columns[0].Width = 20;
+            ultraGrid1.DisplayLayout.Bands[1].Columns[0].Width = 200;
+        }
+
+        public DataSet1 GetData()
+        {
+            DataSet1 ds = new DataSet1();
+
+            DataTable table = ds.Tables["DataTable1"];
+            for (int i = 0; i < 5; i++)
+            {
+                table.Rows.Add(i, "Hello " + i, i);
+            }
+            DataTable table2 = ds.Tables["DataTable2"];
+            for (int i = 0; i < 5; i++)
+            {
+                table2.Rows.Add(i, "Hello " + i % 3, i % 3);
+            }
+
+            return ds;
+        }
+    }
+}
